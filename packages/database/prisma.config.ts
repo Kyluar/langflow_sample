d2dd9d2b837e1dc43ed595528f738c14b3412b81
Gabriel Ribeiro Ramos <104779676+Kyluar@users.noreply.github.com>
2026-01-19 09:05:58 -0300

    :card_file_box: raw: Add env vars for PostGres work

--- packages/database/prisma.config.ts
+++ packages/database/prisma.config.ts
@@ -1,12 +1,23 @@
 import 'dotenv/config'
 import { defineConfig } from 'prisma/config'
 
+const {
+	POSTGRES_USER,
+	POSTGRES_PASSWORD,
+	POSTGRES_HOST,
+	POSTGRES_PORT,
+	POSTGRES_DB,
+	POSTGRES_DB_SCHEMA
+} = process.env
+
+const databaseUrl = `postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB}?schema=${POSTGRES_DB_SCHEMA}`
+
 export default defineConfig({
 	schema: 'prisma/schema.prisma',
 	migrations: {
 		path: 'prisma/migrations'
 	},
 	datasource: {
-		url: process.env.DATABASE_URL
+		url: databaseUrl
 	}
 })
