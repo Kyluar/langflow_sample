3772a5a6d10cdfef38ec5b38668e155d3000e2e8
Gabriel Ribeiro Ramos <104779676+Kyluar@users.noreply.github.com>
2026-01-20 07:59:59 -0300

    :seedling: feat: Add seed script

--- packages/database/prisma.config.ts
+++ packages/database/prisma.config.ts
@@ -1,3 +1,8 @@
+import path from 'node:path'
+import { config } from 'dotenv'
+
+config({ path: path.resolve(import.meta.dirname, '../../.env') })
+
 import 'dotenv/config'
 import { defineConfig } from 'prisma/config'
 
@@ -10,14 +15,17 @@ const {
 	POSTGRES_DB_SCHEMA
 } = process.env
 
-const databaseUrl = `postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB}?schema=${POSTGRES_DB_SCHEMA}`
+const DATABASE_URL = `postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB}?schema=${POSTGRES_DB_SCHEMA}`
 
 export default defineConfig({
 	schema: 'prisma/schema.prisma',
 	migrations: {
-		path: 'prisma/migrations'
+		path: 'prisma/migrations',
+		seed: 'tsx prisma/seed.ts'
 	},
 	datasource: {
-		url: databaseUrl
+		url: DATABASE_URL
 	}
 })
+
+export { DATABASE_URL }
---
+++ packages/database/prisma/seed.ts
@@ -0,0 +1,72 @@
+import { PrismaPg } from '@prisma/adapter-pg'
+import { DATABASE_URL } from '../prisma.config'
+import { type Prisma, PrismaClient } from '../src/generated/prisma/client'
+
+const connectionString = `${DATABASE_URL}`
+
+const adapter = new PrismaPg({ connectionString })
+const prisma = new PrismaClient({ adapter })
+
+const seedUsers: Prisma.UserCreateInput[] = [
+	{
+		name: 'Ana Beatriz Silva',
+		email: 'ana.silva@mock.com'
+	},
+	{
+		name: 'Lucas Oliveira Lima',
+		email: 'lucas.lima@mock.com'
+	},
+	{
+		name: 'Mariana Costa',
+		email: 'mariana.costa@mock.com'
+	},
+	{
+		name: 'Ricardo Augusto Pires',
+		email: 'ricardo.pires@mock.com'
+	},
+	{
+		name: 'Juliana Mendes',
+		email: 'juliana.mendes@mock.com'
+	},
+	{
+		name: 'Enzo Valentim',
+		email: 'enzo.valentim@mock.com'
+	},
+	{
+		name: 'Camila Ferreira',
+		email: 'camila.ferreira@mock.com'
+	},
+	{
+		name: 'Thiago Souza',
+		email: 'thiago.souza@mock.com'
+	},
+	{
+		name: 'Fernanda Rocha',
+		email: 'fernanda.rocha@mock.com'
+	},
+	{
+		name: 'Bruno Henrique Vaz',
+		email: 'bruno.vaz@mock.com'
+	}
+]
+
+async function main() {
+	const transactions = seedUsers.map((u) =>
+		prisma.user.upsert({ where: { email: u.email }, update: {}, create: u })
+	)
+
+	const users = await prisma.$transaction(transactions)
+
+	console.info(users)
+	console.info('Database seeded successfully!')
+}
+
+main()
+	.then(async () => {
+		await prisma.$disconnect()
+	})
+	.catch(async (e) => {
+		console.error(e)
+		await prisma.$disconnect()
+		process.exit(1)
+	})
