d5e33949346b951a50201f3c4de3885fc34e5c97
Kyluar <104779676+Kyluar@users.noreply.github.com>
2026-01-27 13:44:50 -0300

    :bug: fix: Fix the pnpm dev

--- packages/database/src/lib/utils.ts
+++ packages/database/src/lib/utils.ts
@@ -1,8 +1,21 @@
 import { PrismaPg } from '@prisma/adapter-pg'
-import { DATABASE_URL } from '../../prisma.config'
-import { PrismaClient } from '../../src/generated/prisma/client'
+import { PrismaClient } from '../../src/generated/prisma/client.js'
+
+export function generateDatabaseUrl(): string{
+	const {
+		POSTGRES_USER,
+		POSTGRES_PASSWORD,
+		POSTGRES_HOST,
+		POSTGRES_PORT,
+		POSTGRES_DB,
+		POSTGRES_DB_SCHEMA
+	} = process.env
+	
+	return `postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB}?schema=${POSTGRES_DB_SCHEMA}`
+}
 
 export function PrismaClientFactory() {
+	const DATABASE_URL = generateDatabaseUrl()
 	const connectionString = `${DATABASE_URL}`
 	const adapter = new PrismaPg({ connectionString })
 	return new PrismaClient({ adapter })
