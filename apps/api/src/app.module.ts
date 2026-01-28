bed57398556aae41ae2ec368432393c9daea0c56
Gabriel Ribeiro Ramos <104779676+Kyluar@users.noreply.github.com>
2026-01-28 10:32:10 -0300

    :bug: fix: Fix Api initialization

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 import { CustomPrismaModule } from 'nestjs-prisma/dist/custom'
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
 import { config, validate } from './lib/config/env'
 import { prisma } from './lib/extensions/prisma.extension'
 import { UsersModule } from './users/users.module'
@@ -16,13 +14,12 @@ import { UsersModule } from './users/users.module'
 		}),
 		CustomPrismaModule.forRootAsync({
 			name: 'PrismaService',
+			isGlobal: true,
 			useFactory: () => {
 				return prisma
 			}
 		}),
 		UsersModule
-	],
-	controllers: [AppController],
-	providers: [AppService]
+	]
 })
 export class AppModule {}
