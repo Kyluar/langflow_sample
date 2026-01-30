8cbbf4771029710e16e816245fefb5d04ee72944
Kyluar <104779676+Kyluar@users.noreply.github.com>
2026-01-30 16:23:49 -0300

    :bug: fix: Fix the DocumentDto type

--- apps/api/src/document/document.controller.ts
+++ apps/api/src/document/document.controller.ts
@@ -1,47 +1,56 @@
+// biome-ignore-all lint/style/useImportType: Required
 import {
-    Body,
-    Controller,
-    Delete,
-    Get,
-    Param,
-    ParseUUIDPipe,
-    Patch,
-    Post
+	Body,
+	Controller,
+	Delete,
+	Get,
+	Param,
+	ParseUUIDPipe,
+	Patch,
+	Post
 } from '@nestjs/common'
-// biome-ignore lint/style/useImportType: Required
+import { DocumentSchema } from '@repo/schemas'
+import {
+	CreateDocumentDto,
+	UpdateDocumentDto
+} from 'src/lib/types/dto/document'
 import { DocumentService } from './document.service'
-import type { DocumentSchema } from '@repo/schemas'
-import type { CreateDocumentDto, UpdateDocumentDto } from 'src/lib/types/dto/document'
 
 @Controller('document')
 export class DocumentController {
-    constructor(private readonly service: DocumentService) { }
+	constructor(private readonly service: DocumentService) {}
 
-    @Get(':id')
-    getDocumentById(@Param('id', ParseUUIDPipe) id: string): Promise<DocumentSchema> {
-        return this.service.getDocumentById(id)
-    }
+	@Get(':id')
+	getDocumentById(
+		@Param('id', ParseUUIDPipe) id: string
+	): Promise<DocumentSchema> {
+		return this.service.getDocumentById(id)
+	}
 
-    @Get()
-    getDocuments(): Promise<DocumentSchema[]> {
-        return this.service.getDocuments()
-    }
+	@Get()
+	getDocuments(): Promise<DocumentSchema[]> {
+		return this.service.getDocuments()
+	}
 
-    @Post()
-    createDocument(@Body() documentData: CreateDocumentDto): Promise<DocumentSchema> {
-        return this.service.createDocument(documentData)
-    }
+	@Post()
+	createDocument(
+		@Body() documentData: CreateDocumentDto
+	): Promise<DocumentSchema> {
+		return this.service.createDocument(documentData)
+	}
 
-    @Patch(':id')
-    updateDocument(
-        @Param('id', ParseUUIDPipe) id: string,
-        @Body() data: UpdateDocumentDto
-    ): Promise<DocumentSchema> {
-        return this.service.updateDocumentById(id, data)
-    }
+	@Patch(':id')
+	updateDocument(
+		@Param('id', ParseUUIDPipe) id: string,
+		@Body() data: UpdateDocumentDto
+	): Promise<DocumentSchema> {
+		return this.service.updateDocumentById(id, data)
+	}
 
-    @Delete(':id')
-    deleteDocument(@Param('id', ParseUUIDPipe) id: string): Promise<DocumentSchema> {
-        return this.service.deleteDocumentById(id)
-    }
+	@Delete(':id')
+	deleteDocument(
+		@Param('id', ParseUUIDPipe) id: string
+	): Promise<DocumentSchema> {
+		return this.service.deleteDocumentById(id)
+	}
 }
