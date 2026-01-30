c890fda9c46e972e058af9bb027b4e2ac3b517a3
Kyluar <104779676+Kyluar@users.noreply.github.com>
2026-01-30 16:17:19 -0300

    :bug: fix: Fix document schemas imports

--- packages/schemas/src/index.ts
+++ packages/schemas/src/index.ts
@@ -1,4 +1,5 @@
 export { ZodError } from 'zod'
 export * from './config/env.js'
+export * from './document/document.schema.js'
 export * from './user/user.schema.js'
 export { z } from './zod/zod.config.js'
