1a4bfa447dc0ee4ff4b07efa47721c771496e740
Kyluar <104779676+Kyluar@users.noreply.github.com>
2026-01-30 16:32:38 -0300

    :sparkles: feat: Set up Swagger in the api

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,6 +1,7 @@
 // biome-ignore-all lint/correctness/useHookAtTopLevel: Conflict
 import { ConfigService } from '@nestjs/config'
 import { HttpAdapterHost, NestFactory } from '@nestjs/core'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 import { AppModule } from './app.module'
 import { PrismaClientExceptionFilter } from './lib/filters/prisma.exception.filter'
 import { ZodValidationExceptionFilter } from './lib/filters/zod.exception.filter'
@@ -9,8 +10,21 @@ async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
 	const { httpAdapter } = app.get(HttpAdapterHost)
 	const PORT = app.get(ConfigService).getOrThrow('PORT')
+
+	const config = new DocumentBuilder()
+		.setTitle('CTD Resource API')
+		.setDescription('Documentação da API da aplicação CTD Resource')
+		.setVersion('1.0')
+		.build()
+
+	const documentFactory = () => SwaggerModule.createDocument(app, config)
+	SwaggerModule.setup('api', app, documentFactory, {
+		jsonDocumentUrl: 'api/json'
+	})
+
 	app.useGlobalFilters(new ZodValidationExceptionFilter())
 	app.useGlobalFilters(new PrismaClientExceptionFilter(httpAdapter))
+
 	await app.listen(PORT)
 	console.info(`Running on port ${PORT}`)
 }
