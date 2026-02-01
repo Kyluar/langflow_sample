bdc21a8d846981776336df466fceabf26776fd7e
Gabriel Ribeiro Ramos <104779676+Kyluar@users.noreply.github.com>
2026-02-01 09:59:50 -0300

    :mute: cleanup: Remove connetionString log

--- apps/api/src/lib/utils/prisma.utils.ts
+++ apps/api/src/lib/utils/prisma.utils.ts
@@ -3,7 +3,6 @@ import { PrismaClient, PrismaPg } from '@repo/database'
 
 export function PrismaClientFactory(config: ConfigService) {
 	const connectionString = config.getOrThrow('DATABASE_URL')
-	console.log({ connectionString })
 	const adapter = new PrismaPg({ connectionString })
 	return new PrismaClient({ adapter })
 }
