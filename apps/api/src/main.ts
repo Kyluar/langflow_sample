b624afcb4e9dd121f0098f22bb2ae1bd6de1a902
Gabriel Ribeiro Ramos <104779676+Kyluar@users.noreply.github.com>
2026-02-01 13:23:22 -0300

    :lipstick: feat: Customize swagger documentation

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,14 +1,23 @@
 // biome-ignore-all lint/correctness/useHookAtTopLevel: Conflict
+
+import { join } from 'node:path'
 import { ConfigService } from '@nestjs/config'
 import { HttpAdapterHost, NestFactory } from '@nestjs/core'
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+import { NestExpressApplication } from '@nestjs/platform-express'
+import {
+	DocumentBuilder,
+	SwaggerCustomOptions,
+	SwaggerModule
+} from '@nestjs/swagger'
 import { cleanupOpenApiDoc } from 'nestjs-zod'
 import { AppModule } from './app.module'
 import { PrismaClientExceptionFilter } from './lib/filters/prisma.exception.filter'
 import { ZodValidationExceptionFilter } from './lib/filters/zod.exception.filter'
 
 async function bootstrap() {
-	const app = await NestFactory.create(AppModule)
+	const app = await NestFactory.create<NestExpressApplication>(AppModule)
+	app.useStaticAssets(join(__dirname, '..', 'public'))
+
 	const { httpAdapter } = app.get(HttpAdapterHost)
 	const PORT = app.get(ConfigService).getOrThrow('PORT')
 
@@ -17,11 +26,30 @@ async function bootstrap() {
 		.setDescription('Documentação da API da aplicação CTD Resource')
 		.setVersion('1.0')
 		.build()
+
 	const openApiDoc = SwaggerModule.createDocument(app, config)
 
-	SwaggerModule.setup('api', app, cleanupOpenApiDoc(openApiDoc), {
-		jsonDocumentUrl: 'api/json'
-	})
+	const customOptions: SwaggerCustomOptions = {
+		jsonDocumentUrl: 'api/json',
+		customSiteTitle: 'CTD Resource - Docs',
+		customfavIcon: '/favicon.ico',
+		customCss: `
+		.topbar-wrapper .link svg {
+				display: none;
+			}
+		.topbar-wrapper .link::after {
+				content: "";
+				background-image: url('/ctdLogo.svg');
+				background-repeat: no-repeat;
+    			background-position: center left;
+    			background-size: contain;
+				width: 250px;
+    			height: 50px;
+			}
+		`
+	}
+
+	SwaggerModule.setup('api', app, cleanupOpenApiDoc(openApiDoc), customOptions)
 
 	app.useGlobalFilters(new ZodValidationExceptionFilter())
 	app.useGlobalFilters(new PrismaClientExceptionFilter(httpAdapter))
